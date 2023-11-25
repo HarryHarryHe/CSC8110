e99aca9f5e606b1655bc889fff3091fff498a8f6
Harry <874497521@qq.com>
2023-11-25 23:43:05 +0000

    webapi test

--- src/main/java/com/example/loadgenerator/controller/RequestClientController.java
+++ src/main/java/com/example/loadgenerator/controller/RequestClientController.java
@@ -3,8 +3,6 @@ package com.example.loadgenerator.controller;
 import com.example.loadgenerator.service.RequestClientService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +17,10 @@ public class RequestClientController {
 
     @GetMapping("/getTimeout")
     public String getTimeout(){
-        log.info("getTimeout() is executed...");
         return service.getTimeout();
+    }
+    @GetMapping("/get")
+    public String getIt(){
+        return "hello world";
     }
 }
