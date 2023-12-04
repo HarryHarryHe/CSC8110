6376e3735ab13a0f14c769b1f9e1dba660c50700
Harry <874497521@qq.com>
2023-12-04 02:34:15 +0000

    annotation

--- src/main/java/com/example/loadgenerator/constant/MyConstant.java
+++ src/main/java/com/example/loadgenerator/constant/MyConstant.java
@@ -8,16 +8,31 @@ import java.util.concurrent.atomic.AtomicLong;
 @Component
 public class MyConstant {
 
-    public static final long TIMEOUT = 10000;
 
-    public static final String TARGET = System.getenv().getOrDefault("TARGET","https://www.baidu.com");
-    public static final int FREQUENCY = Integer.parseInt(System.getenv().getOrDefault("FREQUENCY","2"));
+    /**
+     * Target request address
+     */
+    public static final String TARGET = System.getenv().getOrDefault("TARGET", "https://www.baidu.com");
+    /**
+     * Requests per second
+     */
+    public static final int FREQUENCY = Integer.parseInt(System.getenv().getOrDefault("FREQUENCY", "2"));
     //    private static Integer REQ_TIMES = Integer.parseInt(System.getenv("REQ_TIMES"));
-    public static Integer TOTAL_REQ_TIMES = 10000;
+    /**
+     * The total number of requests required to limit the program from running forever
+     */
+    public static Integer TOTAL_REQ_TIMES = 100;
+    /**
+     * Requested times
+     */
     public static AtomicInteger REQ_TIMES = new AtomicInteger(0);
-
+    /**
+     * Number of failed request timeouts
+     */
     public static AtomicInteger FAILURE_COUNT = new AtomicInteger(0);
-
+    /**
+     * Total request response time
+     */
     public static AtomicLong TOTAL_RESP_TIME = new AtomicLong(0);
 }
 
